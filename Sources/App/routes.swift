83209ada557d0e1f115355f015294a9cd9dfd6dc
Ben Schultz <bens4lsu@gmail.com>
2023-04-24 12:33:54 -0500

    temporary response in the root to provide links

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,7 +7,21 @@ func routes(_ app: Application, _ passports: Passports, _ settings: Configuratio
     try app.register(collection: APIRouteController(passports: passports, settings: settings, logger: logger))
     
     app.get { req async in
-        "Succesfully connected to Passport Host."
+        
+        
+        let oneEncoded = try BenCrypt.encode("1", keys: settings.cryptKeys).addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+        let twoEncoded = try BenCrypt.encode("2", keys: settings.cryptKeys).addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+        logger.debug("http://localhost:8080/\(oneEncoded)")
+        logger.debug("http://localhost:8080/\(twoEncoded)")
+        
+        return """
+            Successfully connected to passport host.
+        
+            WARNING:  The below information should be removed before deploying assessments for production:
+        
+            https://passports.candjinnovations.com/\(oneEncoded)
+            https://passports.candjinnovations.com/\(twoEncoded)
+        """
         
     }
 }
