21ebace5ec9f7acb70ad08a7390b160abd3feb20
Ben Schultz <bens4lsu@gmail.com>
2023-04-24 13:57:07 -0500

    message page - try again

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,18 +8,21 @@ func routes(_ app: Application, _ passports: Passports, _ settings: Configuratio
     
     app.get { req async throws -> View in
         
+        struct Context: Encodable {
+            let message: String
+        }
         
         let oneEncoded = try BenCrypt.encode("1", keys: settings.cryptKeys).addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
         let twoEncoded = try BenCrypt.encode("2", keys: settings.cryptKeys).addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
         
-        let context = """
+        let context = Context(message: """
             Successfully connected to passport host.
         
             WARNING:  The below information should be removed before deploying assessments for production:
         
             https://passports.candjinnovations.com/\(oneEncoded)
             https://passports.candjinnovations.com/\(twoEncoded)
-        """
+        """)
         
         return try await req.view.render("message", context)
         
